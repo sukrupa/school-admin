73eed7f678b444ce5c62dc8307b7a03b4f48ffa8
pavithra <pavikrish2988@gmail.com>
2011-01-25 11:32:53 +0530

    ron, pavithra, jim: #225 created basic twist project

---
+++ src/test/twist/functional-test/src/org/sukrupa/twist/example/MakeAnAssertion.java
@@ -0,0 +1,20 @@
+package org.sukrupa.twist.example;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+public class MakeAnAssertion {
+
+	public void assertThatIsTrue(boolean input) throws Exception {
+		assertTrue(input + " should be true", input);
+	}
+
+	public void greetingAStudentCalledShouldSay(String studentName, String expectedGreeting) throws Exception {
+		Greeter greeter = new Greeter();
+		
+		String actualGreeting = greeter.greet(studentName);
+		
+		assertEquals(expectedGreeting,actualGreeting);
+	}
+
+}
