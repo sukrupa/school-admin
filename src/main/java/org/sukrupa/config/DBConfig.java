07114b9c76693ac97a2531a8c9752de11045264e
Patric Fornasier <patric.fornasier@gmail.com>
2011-01-25 11:28:52 +0530

    pat, juan: pulled out db config into separate class.

---
+++ src/main/java/org/sukrupa/config/DBConfig.java
@@ -0,0 +1,59 @@
+package org.sukrupa.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@ImportResource("classpath:transaction-config.xml")
+public class DBConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new HibernateTransactionManager(sessionFactory());
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        return sessionFactoryFrom(dataSource(), hibernateProperties());
+    }
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDefaultAutoCommit(true);
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:hsqldb:hsql://localhost/sukrupa");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.put("hibernate.cache.provider_class", "org.hibernate.cache.NoCacheProvider");
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.transaction.factory_class", "org.hibernate.transaction.JDBCTransactionFactory");
+        return properties;
+    }
+
+    private SessionFactory sessionFactoryFrom(DataSource dataSource, Properties hibernateProperties) {
+        try {
+            AnnotationSessionFactoryBean sessionFactory = new AnnotationSessionFactoryBean();
+            sessionFactory.setDataSource(dataSource);
+            sessionFactory.setPackagesToScan(new String[]{"org.sukrupa",});
+            sessionFactory.setHibernateProperties(hibernateProperties);
+            sessionFactory.afterPropertiesSet();
+            return sessionFactory.getObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
