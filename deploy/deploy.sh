7746c27979c19a339430584503d9407ef0cefd05
linda <lgoldste@thoughtworks.com>
2011-01-25 16:06:08 +0530

    linda, prdeep: moved generated files to target and deploy folder inside ops

--- deploy/deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp "lib/*;web-server.jar" org.sukrupa.web.WebServer sukrupa.war	
---
+++ ops/deploy/deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp "lib/*:web-server.jar" org.sukrupa.platform.WebServer sukrupa.war
