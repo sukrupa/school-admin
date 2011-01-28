27e7da36446422626f0ae06bfdecbf1df1bceda1
Juan Ibiapina <juanibiapina@thoughtworks.com>
2011-01-28 16:35:13 +0530

    [juan,karthik] #226 again trying to run the webserver on the staging server.

--- ops/deploy/deploy.sh
+++ ops/deploy/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 sh stop-server.sh
-nohup java -cp "lib/*:web-server.jar" org.sukrupa.app.SchoolAdminApp sukrupa.war &
-echo "I, suhas, should be back at the command line"
-echo "ME TOOOO!!"
+nohup java -cp "lib/*:web-server.jar" org.sukrupa.app.SchoolAdminApp sukrupa.war > file.log &
