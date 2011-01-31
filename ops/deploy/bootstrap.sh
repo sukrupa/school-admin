66b54358c56b2c1a425e13e03e9bb560c22b93f3
Juan Ibiapina <juanibiapina@thoughtworks.com>
2011-01-31 12:33:53 +0530

    [juan,phil] #226 fixed problem with database population scripts.

--- ops/deploy/bootstrap.sh
+++ ops/deploy/bootstrap.sh
@@ -5,5 +5,5 @@ sh stop-server.sh
 nohup java -cp "lib/*:web-server.jar" org.sukrupa.app.SchoolAdminApp sukrupa.war >> sukrupa.log &
 
 BUILD_OVERRIDES="-Dresources.main.dir=install/dbdeploy -Dlib.main.dir=lib -Dops.deploy.sql.dir=install/dbdeploy/sql"
-sh install/ant/bin/ant $BUILD_OVERRIDES -buildfile install/dbdeploy/build.xml db:delete-fake-data $@
-sh install/ant/bin/ant $BUILD_OVERRIDES -buildfile install/dbdeploy/build.xml db:insert-fake-data $@
+sh install/ant/bin/ant $BUILD_OVERRIDES -buildfile install/dbdeploy/build.xml db:delete-fake-data
+sh install/ant/bin/ant $BUILD_OVERRIDES -buildfile install/dbdeploy/build.xml db:insert-fake-data
