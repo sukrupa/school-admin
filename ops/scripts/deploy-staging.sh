e1044022208a3cc9f32a4bbedce45e82de179ca8
Juan Ibiapina <juanibiapina@thoughtworks.com>
2011-01-31 16:57:22 +0530

    [juan,phil] #226 deploy script waits for webserver to start.

--- ops/scripts/deploy-staging.sh
+++ ops/scripts/deploy-staging.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ssh $1@$2 "cd $3; unzip -o $4; sh bootstrap.sh"
-wget --spider http://twu-staging:8080/sukrupa/app/students || exit 1
+sleep 10
+wget --spider http://$2:8080/sukrupa/app/students || exit 1
