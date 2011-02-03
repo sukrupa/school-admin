2510163c26aa7098daae95b685c314d2ab361d80
Pavithra <pavithra@thoughtworks.com>
2011-02-03 11:30:23 +0530

    [Pavithra, Karthik] #163 Modified database schema - added joda date

--- src/test/shared/java/org/sukrupa/event/EventBuilder.java
+++ src/test/shared/java/org/sukrupa/event/EventBuilder.java
@@ -15,53 +15,50 @@ import java.util.Set;
  * To change this template use File | Settings | File Templates.
  */
 public class EventBuilder {
-      private String title;
-      private Date date;
-      private Time time;
-      private String venue;
-      private String coordinator;
-      private String description;
-      private String notes;
-      private Set<Student> attendees;
+    private String title;
+    private String venue;
+    private String coordinator;
+    private String description;
+    private String notes;
+    private Set<Student> attendees;
     private DateTime datetime;
 
-    public EventBuilder title(String title){
+    public EventBuilder title(String title) {
         this.title = title;
         return this;
     }
-    public EventBuilder date(Date date){
-        this.date = date;
-        return this;
-    }
-    public EventBuilder time(Time time){
-        this.time = time;
-        return this;
-    }
+
     public EventBuilder venue(String venue) {
         this.venue = venue;
         return this;
     }
+
     public EventBuilder coordinator(String coordinator) {
         this.coordinator = coordinator;
         return this;
     }
+
     public EventBuilder description(String description) {
         this.description = description;
         return this;
     }
+
     public EventBuilder notes(String notes) {
         this.notes = notes;
         return this;
     }
+
     public EventBuilder attendees(Set<Student> attendees) {
         this.attendees = attendees;
         return this;
     }
+
     public EventBuilder datetime(DateTime dateTime) {
         this.datetime = dateTime;
         return this;
     }
-    public Event build(){
-        return new Event(title,date,time,datetime,venue,coordinator,description,notes,attendees);
+
+    public Event build() {
+        return new Event(title, datetime, venue, coordinator, description, notes, attendees);
     }
 }
--- src/test/unit/java/org/sukrupa/event/EventTest.java
+++ src/test/unit/java/org/sukrupa/event/EventTest.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 import org.sukrupa.student.Student;
 import org.sukrupa.student.StudentBuilder;
 
-import java.sql.Date;
-import java.sql.Time;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,16 +15,16 @@ import static org.hamcrest.Matchers.is;
 public class EventTest {
 
     @Test
-    public void identicalEventsAreEqual(){
-        Event event1 = new EventBuilder().title("Dummy event").datetime(new DateTime(2010, 8, 29,10,10,10,0))
+    public void identicalEventsAreEqual() {
+        Event event1 = new EventBuilder().title("Dummy event").datetime(new DateTime(2010, 8, 29, 10, 10, 10, 0))
                 .venue("DD").coordinator("coord").description("desc").notes("notes").build();
-        Event event2 = new EventBuilder().title("Dummy event").datetime(new DateTime(2010, 8, 29,10,10,10,0))
+        Event event2 = new EventBuilder().title("Dummy event").datetime(new DateTime(2010, 8, 29, 10, 10, 10, 0))
                 .venue("DD").coordinator("coord").description("desc").notes("notes").build();
-        assertThat(event1.equals(event2),is(true));
+        assertThat(event1.equals(event2), is(true));
     }
 
     @Test
-    public void shouldReturnTheCorrectStudentList(){
+    public void shouldReturnTheCorrectStudentList() {
         Student sahil = new StudentBuilder().name("Sahil").studentClass("Nursery").sex("Male").build();
         Student renaud = new StudentBuilder().name("Renaud").studentClass("Nursery").sex("Female").build();
         Student pat = new StudentBuilder().name("pat").religion("n/a").caste("huh?").subCaste("hmm").area("DD").sex("male").dateOfBirth(new LocalDate(1985, 5, 24)).studentClass("4th grade").studentId("abcdef").build();
@@ -34,31 +32,9 @@ public class EventTest {
         attendees.add(sahil);
         attendees.add(renaud);
         attendees.add(pat);
-        Event event1 = new EventBuilder().title("Dummy event").date(new Date(2010, 8, 29)).time(new Time(10, 10, 10))
-                .venue("DD").coordinator("coord").description("desc").notes("notes").attendees(attendees).build();
-        assertThat(attendees.equals(event1.getAttendees()),is(true));
+        Event event1 = new EventBuilder().title("Dummy event").datetime(new DateTime(2010, 8, 29, 10, 10, 10, 0)).venue("DD").coordinator("coord").description("desc").notes("notes").attendees(attendees).build();
+        assertThat(attendees.equals(event1.getAttendees()), is(true));
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
