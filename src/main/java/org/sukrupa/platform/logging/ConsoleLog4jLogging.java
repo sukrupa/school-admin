95f6acb7904079f73f1fe5d211890a3fa20a7bf3
Patric Fornasier <patric.fornasier@gmail.com>
2011-02-06 11:31:18 +0530

    [Pat] less noise in build output.

--- src/main/java/org/sukrupa/platform/logging/ConsoleLog4jLogging.java
+++ src/main/java/org/sukrupa/platform/logging/ConsoleLog4jLogging.java
@@ -5,19 +5,24 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 
+import static org.apache.log4j.Level.INFO;
+import static org.apache.log4j.Level.WARN;
+import static org.apache.log4j.Logger.getLogger;
+import static org.apache.log4j.Logger.getRootLogger;
+
 public class ConsoleLog4jLogging {
 
-    private static final Level LOG_LEVEL = Level.INFO;
+    private static final Level DEFAULT_LOG_LEVEL = WARN;
 
     private static final String LOG_PATTERN = "[%t] %-5p %c %x - %m%n";
 
     public static void configureLogging() {
-        configureRootLogger();
+        configure(getRootLogger(), DEFAULT_LOG_LEVEL);
+        configure(getLogger("org.sukrupa"), INFO);
     }
 
-    private static void configureRootLogger() {
-        Logger rootLogger = Logger.getRootLogger();
-        rootLogger.setLevel(LOG_LEVEL);
-        rootLogger.addAppender(new ConsoleAppender(new PatternLayout(LOG_PATTERN)));
+    private static void configure(Logger logger, Level level) {
+        logger.setLevel(level);
+        logger.addAppender(new ConsoleAppender(new PatternLayout(LOG_PATTERN)));
     }
 }
