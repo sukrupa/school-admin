7cfc7167092d7ab6aaf4903ba93c743757f56962
Renaud Tircher <rtircher@thoughtworks.com>
2011-02-07 13:13:59 +0530

    Revert "Merge branch 'master' of github.com:ThoughtWorksDonateNow2/sukrupa"
    This reverts commit b819189bcfd242b3742df6213e2df2ca2bd209f6, reversing
    changes made to 8cf5f7762367b62aa2c5b76480864c2071c50877.

--- src/main/java/org/sukrupa/event/EventRepository.java
+++ src/main/java/org/sukrupa/event/EventRepository.java
@@ -1,7 +1,5 @@
 package org.sukrupa.event;
 
-import com.google.common.base.Splitter;
-import com.google.common.collect.Sets;
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Criterion;
@@ -11,16 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.sukrupa.student.Student;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public class EventRepository {
 
-	private static final String STUDENT_ID = "studentId";
-	static final String ATTENDEES_SEPARATOR = ",";
-
-	private SessionFactory sessionFactory;
+    private SessionFactory sessionFactory;
 
     @Autowired
     public EventRepository(SessionFactory sessionFactory) {
@@ -31,22 +26,19 @@ public class EventRepository {
         return sessionFactory.getCurrentSession().createCriteria(Event.class).list();
     }
 
-    public void save(EventRecord eventRecord) {
-	    Set<Student> attendees = retrieveStudent(eventRecord.getAttendees());
-	    sessionFactory.getCurrentSession().save(Event.createFrom(eventRecord, attendees));
+    public void save(Event event) {
+        sessionFactory.getCurrentSession().save(event);
     }
 
-    private Set<Student> retrieveStudent(String studentIds) {
+    public List<Student> retrieveStudent(String... studentIDs) {
+        List <String> studentIDList = new ArrayList<String>();
+        for(String studentID : studentIDs)
+            studentIDList.add(studentID);
         Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Student.class);
-        Criterion attendee = Restrictions.in(STUDENT_ID, createStudentIds(studentIds));
+        Criterion attendee = Restrictions.in("studentId",studentIDList);
         Disjunction disjunction = Restrictions.disjunction();
         disjunction.add(attendee);
         criteria.add(disjunction);
-        return Sets.newHashSet(criteria.list());
+        return criteria.list();
     }
-
-	private Set<String> createStudentIds(String studentIds) {
-		return Sets.newLinkedHashSet(Splitter.on(ATTENDEES_SEPARATOR).omitEmptyStrings().trimResults().split(studentIds));
-	}
-
 }
