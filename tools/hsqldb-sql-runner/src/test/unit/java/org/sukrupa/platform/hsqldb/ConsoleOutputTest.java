dfe58ff1e4d69ca721e8a855acb87ae5099ef1fe
Jim Barritt <jim@planet-ix.com>
2011-02-07 21:51:49 +0530

    [Jim] #225 Testing console output - step 2 - Extract attatch method

---
+++ tools/hsqldb-sql-runner/src/test/unit/java/org/sukrupa/platform/hsqldb/ConsoleOutputTest.java
@@ -0,0 +1,43 @@
+package org.sukrupa.platform.hsqldb;
+
+import org.junit.*;
+
+import java.io.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ConsoleOutputTest {
+
+    private final SystemOutRecorder systemOutRecorder = new SystemOutRecorder();
+
+    @Before
+    public void setUp() throws Exception {
+        attatch();
+    }
+
+    private void attatch() throws UnsupportedEncodingException {
+        systemOutRecorder.bos = new ByteArrayOutputStream();
+        systemOutRecorder.out = new PrintStream(systemOutRecorder.bos, SystemOutRecorder.AUTO_FLUSH, "UTF-8");
+        systemOutRecorder.previousOut = System.out;
+        System.setOut(systemOutRecorder.out);
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(systemOutRecorder.previousOut);
+    }
+
+    @Test
+    public void writes_a_line_to_std_out() throws Exception {
+        new ConsoleOutput().println("Hello world");
+
+        String output = systemOutRecorder.getOutput();
+
+        assertThat(output, is("Hello world\n"));
+    }
+
+    private String getOutput() throws UnsupportedEncodingException {
+        return systemOutRecorder.getOutput();
+    }
+}
