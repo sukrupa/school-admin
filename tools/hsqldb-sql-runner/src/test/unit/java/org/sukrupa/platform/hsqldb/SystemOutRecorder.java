8c112e1c1d5b8629b82271a834de2fd622ebb681
Jim Barritt <jim@planet-ix.com>
2011-02-07 21:58:22 +0530

    [Jim] #225 Refactor Console output step 6 : tidy up SystemOutRecorder

--- tools/hsqldb-sql-runner/src/test/unit/java/org/sukrupa/platform/hsqldb/SystemOutRecorder.java
+++ tools/hsqldb-sql-runner/src/test/unit/java/org/sukrupa/platform/hsqldb/SystemOutRecorder.java
@@ -3,26 +3,24 @@ package org.sukrupa.platform.hsqldb;
 import java.io.*;
 
 public class SystemOutRecorder {
-    static final boolean AUTO_FLUSH = true;
-    PrintStream previousOut;
-    PrintStream out;
-    ByteArrayOutputStream bos;
+    private static final boolean AUTO_FLUSH = true;
 
-    public SystemOutRecorder() {
-    }
+    private PrintStream out;
+    private ByteArrayOutputStream bos;
+    private PrintStream previousOut;
 
-    String getOutput() throws UnsupportedEncodingException {
-        return bos.toString("UTF-8");
-    }
-
-    void attatch() throws UnsupportedEncodingException {
+    public void attatch() throws UnsupportedEncodingException {
         bos = new ByteArrayOutputStream();
         out = new PrintStream(bos, AUTO_FLUSH, "UTF-8");
         previousOut = System.out;
         System.setOut(out);
     }
 
-    void detatch() {
+    public String getOutput() throws UnsupportedEncodingException {
+        return bos.toString("UTF-8");
+    }
+
+    public void detatch() {
         System.setOut(previousOut);
     }
 }
