ff99af0528cd698624790a896e5c668b0f74f336
Renaud Tircher <rtircher@thoughtworks.com>
2011-02-07 23:35:23 +0530

    [Karthik, Renaud] #163 UI Field validation

--- src/web/scripts/recordEvent.js
+++ src/web/scripts/recordEvent.js
@@ -1,3 +1,25 @@
 function resetFields() {
-    $('#title').text("");
+    $('#title').val("");
+    $('#date').val("");
+    $('#time').val("");
+    $('#venue').val("");
+    $('#description').val("");
+    $('#coordinator').val("");
+    $('#attendees').val("");
+    $('#notes').val("");
+}
+
+function validateMandatoryFields() {
+    if ($('#title').val() === "" || $('#date').val() === "" || $('#time').val() === ""
+    		|| $('#description').val() === ""  || $('#attendees').val() === "" ) {
+    	$('#errorMessages').html("data should be entered in fields title,date,time,attendees and description");
+	} else {
+		if (verifyDateFormat()) {
+        	$('#eventRecord').submit();
+        }
+	}
+}
+
+function verifyDateFormat(){
+	return true;
 }
