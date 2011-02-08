b2ec2a17f27b8612054feacb77aa33326296c4e3
Renaud Tircher <rtircher@thoughtworks.com>
2011-02-08 22:57:19 +0530

    [Renaud] #213Changed date validation to use JQuery

--- src/web/scripts/studentUpdate.js
+++ src/web/scripts/studentUpdate.js
@@ -1,10 +1,12 @@
-function validateDate() {
-	var dateStr = $('#dateOfBirth').val();
-	var dummyTimeStr = "01:01";
+$(document).ready(function (){
+	$('#dateOfBirth').keyup(function (){
+		var dateStr = $('#dateOfBirth').val();
+		var dummyTimeStr = "01:01";
 
-	if (!new DateValidator().validate(dateStr, dummyTimeStr)) {
-        $('#dateErrorMessage').html("Invalid date.  Format: dd/mm/yyyy");
-	} else {
-		$('#dateErrorMessage').html("");
-	}
-}
+		if (!new DateValidator().validate(dateStr, dummyTimeStr)) {
+			$('#dateErrorMessage').html("Invalid date.  Format: dd/mm/yyyy");
+		} else {
+			$('#dateErrorMessage').html("");
+		}
+	});
+});
