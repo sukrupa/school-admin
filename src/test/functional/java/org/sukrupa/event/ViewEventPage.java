ac61ee0ace6afcece4abb6fd0d0c5ab8e7456861
Suhas Vishwanath <suhas.vishwanath@thoughtworks.com>
2011-02-08 11:18:32 +0530

    [Rebecca, Suhas] #262 Wrote failing functional test for view event page

---
+++ src/test/functional/java/org/sukrupa/event/ViewEventPage.java
@@ -0,0 +1,32 @@
+package org.sukrupa.event;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.sukrupa.app.students.StudentRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ViewEventPage {
+    private WebDriver driver;
+
+    public ViewEventPage() {
+        this.driver = new HtmlUnitDriver();
+        driver.get("http://localhost:8080/sukrupa/app/event/" + 1);
+    }
+
+
+    @Test
+    @Ignore("[Rebecca, Suhas] WIP")
+    public void testGetEvent(){
+        String title = driver.findElements(By.xpath("//p[@class='title']")).get(0).getText();
+        assertThat(title, is("Fake event_title"));
+    }
+}
