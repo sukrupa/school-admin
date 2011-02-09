117d691cbc9d44061647b3e82fbfa3c336e0fec5
Minno <mndang@thoughtworks.com>
2011-02-09 22:36:04 +0530

    [Minno, Juan] #203 add importing of talents from excel spreadsheets

--- tools/import-student-records/sql_generator.rb
+++ tools/import-student-records/sql_generator.rb
@@ -3,10 +3,16 @@ require 'rubygems'
 
 class SQLGenerator
   
-  def generate_sql(students)
+  def generate_sql(students_and_talents_array)
       sql_statements = []
-      students.each do |student|
+      students_and_talents_array.each do |student_and_talents|
+        student = student_and_talents[0]
+        talents = student_and_talents[1]
         sql_statements << "INSERT INTO student (#{student.attribute_names.join(',')}) VALUES (#{values_for student});"
+        talents.talents.each do |talent|
+          sql_statements << "INSERT INTO student_talent (student_id,talent_id)
+          SELECT student.id,talent.id FROM STUDENT,TALENT WHERE student.student_id = '#{student.student_id}' AND (talent.description = '#{talent}');"
+        end
       end
       sql_statements
   end
