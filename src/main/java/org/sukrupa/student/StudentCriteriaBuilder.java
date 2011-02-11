f48df9746021fa0378d383649682cca11272ed61
Philip Potter <philipgpotter@gmail.com>
2011-02-11 09:55:52 +0530

    [Philip] #261 pull out StudentCriteriaBuilder class from StudentRepository

---
+++ src/main/java/org/sukrupa/student/StudentCriteriaBuilder.java
@@ -0,0 +1,94 @@
+package org.sukrupa.student;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Conjunction;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.joda.time.LocalDate;
+
+public class StudentCriteriaBuilder {
+    private final SessionFactory sessionFactory;
+
+    private static final String STUDENT_CLASS = "studentClass";
+    private static final String GENDER = "gender";
+    private static final String CASTE = "caste";
+    private static final String COMMUNITY_LOCATION = "communityLocation";
+    private static final String NAME = "name";
+    private static final String DATE_OF_BIRTH = "dateOfBirth";
+
+    private static final String TALENTS = "talents";
+    private static final String RELIGION = "religion";
+    private static final String DESCRIPTION = "description";
+
+    public StudentCriteriaBuilder(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public Criteria countMatchingResultsCriteria(StudentSearchParameter searchParam) {
+        return generateSearchCriteria(searchParam).setProjection(Projections.rowCount());
+    }
+
+    public Criteria getPageCriteria(StudentSearchParameter searchParam) {
+        Criteria getPageCriteria = addOrderCriteria(generateSearchCriteria(searchParam));
+
+        int firstIndex = (searchParam.getPage() - 1) * StudentRepository.NUMBER_OF_STUDENTS_TO_LIST_PER_PAGE;
+        getPageCriteria.setFirstResult(firstIndex);
+        getPageCriteria.setMaxResults(StudentRepository.NUMBER_OF_STUDENTS_TO_LIST_PER_PAGE);
+        return getPageCriteria;
+    }
+
+    private Criteria generateSearchCriteria(StudentSearchParameter searchParam) {
+        Conjunction conjunction = createConjunction(searchParam.getStudentClass(), searchParam.getGender(),
+                searchParam.getCaste(), searchParam.getCommunityLocation(), searchParam.getReligion());
+        if (!searchParam.getAgeFrom().isEmpty()) {
+            addAgeCriteria(Integer.parseInt(searchParam.getAgeFrom()), Integer.parseInt(searchParam.getAgeTo()), conjunction);
+        }
+
+        Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Student.class);
+        criteria.add(conjunction);
+        addTalentsSearchCriteria(criteria, searchParam.getTalent());
+        return criteria;
+    }
+
+    private void addAgeCriteria(int ageFrom, int ageTo, Conjunction conjunction) {
+        LocalDate birthDateFrom = computeBirthDateFromAge(ageFrom);
+        LocalDate birthDateTo = computeBirthDateFromAge(getInclusiveUpperBoundAge(ageTo));
+        conjunction.add(Restrictions.between(DATE_OF_BIRTH, birthDateTo, birthDateFrom));
+    }
+
+    private int getInclusiveUpperBoundAge(int ageTo) {
+        return ageTo + 1;
+    }
+
+    private void addTalentsSearchCriteria(Criteria criteria, String talent) {
+        if (!talent.isEmpty()) {
+            criteria.createCriteria(TALENTS).add(Restrictions.eq(DESCRIPTION, talent));
+        }
+    }
+
+    private LocalDate computeBirthDateFromAge(int age) {
+        return new LocalDate().minusYears(age);
+    }
+
+    private Criteria addOrderCriteria(Criteria criteria) {
+        return criteria.addOrder(Order.asc(GENDER).ignoreCase()).addOrder(Order.asc(NAME).ignoreCase());
+    }
+
+    private Conjunction createConjunction(String studentClass, String gender, String caste, String communityLocation, String religion) {
+        Conjunction conjunction = Restrictions.conjunction();
+        addRestrictionIfNotEmpty(STUDENT_CLASS, studentClass, conjunction);
+        addRestrictionIfNotEmpty(GENDER, gender, conjunction);
+        addRestrictionIfNotEmpty(CASTE, caste, conjunction);
+        addRestrictionIfNotEmpty(COMMUNITY_LOCATION, communityLocation, conjunction);
+        addRestrictionIfNotEmpty(RELIGION, religion, conjunction);
+        return conjunction;
+    }
+
+    private void addRestrictionIfNotEmpty(String field, String parameter, Conjunction conjunction) {
+        if (!parameter.isEmpty()) {
+            conjunction.add(Restrictions.eq(field, parameter));
+        }
+    }
+}
--- src/main/java/org/sukrupa/student/StudentRepository.java
+++ src/main/java/org/sukrupa/student/StudentRepository.java
@@ -6,7 +6,6 @@ import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.classic.Session;
 import org.hibernate.criterion.*;
-import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -18,23 +17,16 @@ import static com.google.common.collect.Sets.newHashSet;
 @Repository
 public class StudentRepository {
 
-    private static final String STUDENT_CLASS = "studentClass";
-    private static final String GENDER = "gender";
-    private static final String CASTE = "caste";
-    private static final String COMMUNITY_LOCATION = "communityLocation";
-    private static final String NAME = "name";
-    private static final String DATE_OF_BIRTH = "dateOfBirth";
     private static final String STUDENT_ID = "studentId";
-    private static final String TALENTS = "talents";
-    private static final String DESCRIPTION = "description";
-    private static final String RELIGION = "religion";
     static final int NUMBER_OF_STUDENTS_TO_LIST_PER_PAGE = 5;
 
     private final SessionFactory sessionFactory;
+    private final StudentCriteriaBuilder studentCriteriaBuilder;
 
     @Autowired
     public StudentRepository(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
+        studentCriteriaBuilder = new StudentCriteriaBuilder(this.sessionFactory);
     }
 
 //    @SuppressWarnings("unchecked")
@@ -57,72 +49,13 @@ public class StudentRepository {
     }
 
     public StudentListPage parametricSearch(StudentSearchParameter searchParam) {
-        Criteria countCriteria = generateSearchCriteria(searchParam);
+        Criteria getPageCriteria = studentCriteriaBuilder.getPageCriteria(searchParam);
 
-        int firstIndex = (searchParam.getPage() - 1) * NUMBER_OF_STUDENTS_TO_LIST_PER_PAGE;
-
-        int totalNumberOfResults = ((Number) countCriteria.setProjection(Projections.rowCount()).uniqueResult()).intValue();
+        Criteria countCriteria = studentCriteriaBuilder.countMatchingResultsCriteria(searchParam);
+        int totalNumberOfResults = ((Number) countCriteria.uniqueResult()).intValue();
         int totalNumberOfPages = (totalNumberOfResults - 1) / NUMBER_OF_STUDENTS_TO_LIST_PER_PAGE + 1;
 
-        Criteria getPageCriteria = addOrderCriteria(generateSearchCriteria(searchParam));
-        getPageCriteria.setFirstResult(firstIndex);
-        getPageCriteria.setMaxResults(NUMBER_OF_STUDENTS_TO_LIST_PER_PAGE);
-
-
         return new StudentListPage(getPageCriteria.list(), searchParam.getPage(), totalNumberOfPages);
-    }
-
-    private Criteria generateSearchCriteria(StudentSearchParameter searchParam) {
-        Conjunction conjunction = createConjunction(searchParam.getStudentClass(), searchParam.getGender(),
-                searchParam.getCaste(), searchParam.getCommunityLocation(), searchParam.getReligion());
-        if (!searchParam.getAgeFrom().isEmpty()) {
-            addAgeCriteria(Integer.parseInt(searchParam.getAgeFrom()), Integer.parseInt(searchParam.getAgeTo()), conjunction);
-        }
-
-        Criteria criteria = session().createCriteria(Student.class);
-        criteria.add(conjunction);
-        addTalentsSearchCriteria(criteria, searchParam.getTalent());
-        return criteria;
-    }
-
-    private void addAgeCriteria(int ageFrom, int ageTo, Conjunction conjunction) {
-        LocalDate birthDateFrom = computeBirthDateFromAge(ageFrom);
-        LocalDate birthDateTo = computeBirthDateFromAge(getInclusiveUpperBoundAge(ageTo));
-        conjunction.add(Restrictions.between(DATE_OF_BIRTH, birthDateTo, birthDateFrom));
-    }
-
-    private int getInclusiveUpperBoundAge(int ageTo) {
-        return ageTo + 1;
-    }
-
-    private void addTalentsSearchCriteria(Criteria criteria, String talent) {
-        if (!talent.isEmpty()) {
-            criteria.createCriteria(TALENTS).add(Restrictions.eq(DESCRIPTION, talent));
-        }
-    }
-
-    private LocalDate computeBirthDateFromAge(int age) {
-        return new LocalDate().minusYears(age);
-    }
-
-    private Criteria addOrderCriteria(Criteria criteria) {
-        return criteria.addOrder(Order.asc(GENDER).ignoreCase()).addOrder(Order.asc(NAME).ignoreCase());
-    }
-
-    private Conjunction createConjunction(String studentClass, String gender, String caste, String communityLocation, String religion) {
-        Conjunction conjunction = Restrictions.conjunction();
-        addRestrictionIfNotEmpty(STUDENT_CLASS, studentClass, conjunction);
-        addRestrictionIfNotEmpty(GENDER, gender, conjunction);
-        addRestrictionIfNotEmpty(CASTE, caste, conjunction);
-        addRestrictionIfNotEmpty(COMMUNITY_LOCATION, communityLocation, conjunction);
-        addRestrictionIfNotEmpty(RELIGION, religion, conjunction);
-        return conjunction;
-    }
-
-    private void addRestrictionIfNotEmpty(String field, String parameter, Conjunction conjunction) {
-        if (!parameter.isEmpty()) {
-            conjunction.add(Restrictions.eq(field, parameter));
-        }
     }
 
     public Student update(UpdateStudentParameter studentParam) {
