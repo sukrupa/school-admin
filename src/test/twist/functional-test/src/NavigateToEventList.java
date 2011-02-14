e59b1042d0ce785e8b5f7181c6d7cdc7d9b00844
Dani Schufeldt <dani.schufeldt@gmail.com>
2011-02-14 18:04:01 +0530

    [Dani] #277 Work in progress: Viewing Event Records

---
+++ src/test/twist/functional-test/src/NavigateToEventList.java
@@ -0,0 +1,20 @@
+
+import net.sf.sahi.client.Browser;
+
+public class NavigateToEventList {
+
+	private Browser browser;
+
+	public NavigateToEventList(Browser browser) {
+		this.browser = browser;
+	}
+
+	public void setUp() throws Exception {
+		//This method is executed before the scenario execution starts.
+	}
+
+	public void tearDown() throws Exception {
+		//This method is executed after the scenario execution finishes.
+	}
+
+}
