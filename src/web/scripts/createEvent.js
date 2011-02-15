48ab59386b01ab060f2f77955843af5fc3c39cfb
juanibiapina <juanibiapina@thoughtworks.com>
2011-02-15 19:03:49 +0530

    [Juan, Karthik] #163 added javascript validation for required fields on create event

--- src/web/scripts/createEvent.js
+++ src/web/scripts/createEvent.js
@@ -1,8 +1,30 @@
 $(document).ready(function () {
+    $('#createEvent').submit(function () {
+        if (validateFields()) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
     $('#clear').click(function () {
         resetFields();
     });
 });
+
+function validateFields() {
+    var valid = true;
+    var errorMessage = "";
+
+    if ($('#title').val() === "" || $('#description').val() === "" || $('#date').val() === "" || $('#attendees').val() === "") {
+        errorMessage += "Please fill in all required fields.";
+        valid = false;
+    }
+
+    $('#errorMessages').html(errorMessage);
+
+    return valid;
+}
 
 function resetFields() {
     $('#title').val("");
