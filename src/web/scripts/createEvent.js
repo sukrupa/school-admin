74599cfe9dbeb00f64495c7fa3d87019ec3f7e83
juanibiapina <juanibiapina@thoughtworks.com>
2011-02-15 19:13:36 +0530

    [Juan, Karthik] #163 added javascript validation for date format on create event

--- src/web/scripts/createEvent.js
+++ src/web/scripts/createEvent.js
@@ -15,6 +15,14 @@ $(document).ready(function () {
 function validateFields() {
     var valid = true;
     var errorMessage = "";
+    var dateStr = $('#date').val();
+    var dummyTimeStr = "01:01";
+
+    if (!new DateValidator().validate(dateStr, dummyTimeStr)) {
+        $('#dateErrorMessage').html("Invalid date.  Format: dd-mm-yyyy.");
+        errorMessage += "Please insert a valid date (Format: dd-mm-yyyy).<br/>";
+        valid = false;
+    }
 
     if ($('#title').val() === "" || $('#description').val() === "" || $('#date').val() === "" || $('#attendees').val() === "") {
         errorMessage += "Please fill in all required fields.";
