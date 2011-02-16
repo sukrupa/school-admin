6ce00664725655db2b92347be87d866b868e7b01
Minno <mndang@thoughtworks.com>
2011-02-16 17:40:22 +0530

    [Minno, Rebecca, Suhas, Juan] recovered deleted files

---
+++ src/main/java/org/sukrupa/student/AnnualUpdateController.java
@@ -0,0 +1,15 @@
+package org.sukrupa.student;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value ="/annualupdate")
+public class AnnualUpdateController {
+
+    @RequestMapping
+    public String annualUpdate() {
+
+        return "annualUpdate";
+  }
+}
