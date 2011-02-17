64984326a6fb6a691ba8c6a867a4c00711b7617a
Renaud Tircher <rtircher@thoughtworks.com>
2011-02-17 12:05:43 +0530

    [Renaud] #CLEANING - Removed dynamic message for invalid date (while typing) and add format of date in dob label

--- src/web/scripts/studentUpdate.js
+++ src/web/scripts/studentUpdate.js
@@ -1,15 +1,4 @@
 $(document).ready(function () {
-    $('#dateOfBirth').keyup(function () {
-        var dateStr = $('#dateOfBirth').val();
-        var dummyTimeStr = "01:01";
-
-        if (!new DateValidator().validate(dateStr, dummyTimeStr)) {
-            $('#dateErrorMessage').html("Invalid date.  Format: dd-mm-yyyy");
-        } else {
-            $('#dateErrorMessage').html("");
-        }
-    });
-
     $('#save').click(function () {
         if (validateFields()) {
             $('#updateStudent').submit();
