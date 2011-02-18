e5263aa65c4bc656f1f1f41f4e189a2c7bc1866b
Philip Potter <philipgpotter@gmail.com>
2011-02-18 10:22:56 +0530

    [Philip, Juan] #300 shell script to run JS tests

---
+++ spike-js-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+discoverFirefoxCommand() {
+    local FIREFOX_BINARY
+    case $(uname) in
+      Linux)
+        FIREFOX_BINARY="firefox"
+        ;;
+      Darwin)
+        FIREFOX_BINARY="/Applications/Firefox.app/Contents/MacOS/firefox-bin"
+        ;;
+      *)
+       echo "Unrecognised OS, supported OS's are MacOSX, Linux & SunOS (Solaris)"
+       exit -1
+    esac
+
+    echo "$FIREFOX_BINARY"
+}
+
+FIREFOX=$(discoverFirefoxCommand)
+
+java -jar lib/JsTestDriver.jar --port 4224 --browser $FIREFOX --tests all
