bd77904a1577ab27b25140f9b3679f12cd217167
Jim Barritt <jim@planet-ix.com>
2011-03-20 03:35:51 +0530

    [JimB] #CLEANING - Getting Ruby up and running in IntelliJ

--- tools/import-student-records/test/talent_test.rb
+++ tools/import-student-records/test/talent_test.rb
@@ -2,20 +2,20 @@ require 'test/unit'
 require 'talent'
 
 class TalentTest < Test::Unit::TestCase
-  
+
   def test_should_return_right_amount_of_talents
     talents = Talent.new("Sports,Art")
-    assert_equal(2,talents.talents.length)
+    assert_equal(2, talents.talents.length)
   end
-  
+
   def test_should_return_nil_talent_list
     talents = Talent.new(" ")
     assert_equal([], talents.talents)
   end
-  
+
   def test_should_return_correct_value
     talents = Talent.new("Sports,Art,Drawing")
     assert_equal("Arts & Crafts", talents.talents[1])
   end
-  
+
 end
