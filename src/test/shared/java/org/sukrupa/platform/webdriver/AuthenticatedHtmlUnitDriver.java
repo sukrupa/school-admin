b67175aea99e6c973e5c7f90d97c34e0e01b1f5b
Jim Barritt <jim@planet-ix.com>
2011-03-29 04:44:36 +0530

    [JimB] #CLEANING Got an Authenticated webdriver to work - adding new files

--- src/test/shared/java/org/sukrupa/platform/webdriver/AuthenticatedHtmlUnitDriver.java
+++ src/test/shared/java/org/sukrupa/platform/webdriver/AuthenticatedHtmlUnitDriver.java
@@ -1,4 +1,31 @@
 package org.sukrupa.platform.webdriver;
 
-public class AuthenticatedHtmlUnitDriver {
+import com.gargoylesoftware.htmlunit.*;
+import org.apache.log4j.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.htmlunit.*;
+
+public class AuthenticatedHtmlUnitDriver extends HtmlUnitDriver {
+    private static final Logger LOG = Logger.getLogger(AuthenticatedHtmlUnitDriver.class);
+    private static String USERNAME;
+    private static String PASSWORD;
+
+    public AuthenticatedHtmlUnitDriver() {
+    }
+
+    @Override
+    protected WebClient newWebClient(BrowserVersion browserVersion) {
+        LOG.debug("Logging in with: [username:" + USERNAME + "]");
+        WebClient client = super.newWebClient(browserVersion);
+        DefaultCredentialsProvider provider = new DefaultCredentialsProvider();
+        provider.addCredentials(USERNAME, PASSWORD);
+        client.setCredentialsProvider(provider);
+        return client;
+    }
+
+    public static WebDriver authenticatedDriver(String username, String password) {
+        USERNAME = username;
+        PASSWORD = password;
+        return new AuthenticatedHtmlUnitDriver();
+    }
 }
