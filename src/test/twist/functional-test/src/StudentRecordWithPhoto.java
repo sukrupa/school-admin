63354477906540537fb1072fe92e8e8ca4354d71
Balaji <balaji@balaji-Latitude-E6500.(none)>
2011-03-30 16:34:25 +0530

    [BalajiN, PeterM] #408 Added missing files to commit

---
+++ src/test/twist/functional-test/src/StudentRecordWithPhoto.java
@@ -0,0 +1,14 @@
+
+// JUnit Assert framework can be used for verification
+
+import net.sf.sahi.client.Browser;
+
+public class StudentRecordWithPhoto {
+
+	private Browser browser;
+
+	public StudentRecordWithPhoto(Browser browser) {
+		this.browser = browser;
+	}
+
+}
