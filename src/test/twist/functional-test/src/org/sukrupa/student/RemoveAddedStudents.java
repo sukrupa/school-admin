f040cd4cdb9a0bd649d132724500c7b3935b7557
Yael Sembira <ysembira@thoughtworks.com>
2011-04-01 16:10:28 +0530

    [YaelS, JimB] #200 HId add student link from sidebar, got checking of talent checkbox in twist to work plus submit button, sorted out hamcrest on the classpath of eclipse

---
+++ src/test/twist/functional-test/src/org/sukrupa/student/RemoveAddedStudents.java
@@ -0,0 +1,21 @@
+package org.sukrupa.student;
+
+import net.sf.sahi.client.Browser;
+
+public class RemoveAddedStudents {
+
+	private Browser browser;
+
+	public RemoveAddedStudents(Browser browser) {
+		this.browser = browser;
+	}
+
+	public void setUp() throws Exception {
+		// Create something to store the student ids
+	}
+
+	public void tearDown() throws Exception {
+		// remove the added student ids
+	}
+
+}
