5f77b7515d171809d6cd213cd63893dc14e07264
Balaji <balaji@balaji-Latitude-E6500.(none)>
2011-04-04 13:48:58 +0530

    [BalajiN, UgochiO] #367 resolving merge conflicts

--- src/test/shared/java/org/sukrupa/student/StudentBuilder.java
+++ src/test/shared/java/org/sukrupa/student/StudentBuilder.java
@@ -25,16 +25,10 @@ public class StudentBuilder {
     private LocalDate dateOfBirth = new LocalDate();
     private Set<Note> notes = new HashSet<Note>();
     private String imageLink;
-<<<<<<< HEAD
     private StudentStatus status = StudentStatus.ACTIVE;
     private String disciplinary;
     private String performance;
     private  Profile profile;
-
-=======
-    private StudentStatus status;
->>>>>>> master
-
     public StudentBuilder name(String name) {
         this.name = name;
         return this;
@@ -145,11 +139,7 @@ public class StudentBuilder {
     }
 
     public Student build() {
-<<<<<<< HEAD
-        return new Student(studentId, name, religion, caste, subCaste, area, gender, studentClass, talents, father, mother, dateOfBirth, notes, imageLink, StudentStatus.NOT_SET, disciplinary, performance, profile);
-=======
-        return new Student(studentId, name, religion, caste, subCaste, area, gender, studentClass, talents, father, mother, dateOfBirth, notes, imageLink, status);
->>>>>>> master
+       return new Student(studentId, name, religion, caste, subCaste, area, gender, studentClass, talents, father, mother, dateOfBirth, notes, imageLink, StudentStatus.NOT_SET, disciplinary, performance, profile);
     }
 
     public StudentBuilder imageLink(String imageLink) {
