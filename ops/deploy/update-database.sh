db41b8610abb6a940c1afe442e8e2462780db351
Yael Sembira <ysembira@thoughtworks.com>
2011-04-06 16:21:59 +0530

    [BalajiN, FrankT] £408 moving the build file from install/dbdeploy to install

--- ops/deploy/update-database.sh
+++ ops/deploy/update-database.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BUILD_OVERRIDES="-Dresources.main.dir=install -Dlib.main.dir=lib -Dsrc.sql.dir=install/dbdeploy/sql"
-sh install/ant/bin/ant $BUILD_OVERRIDES -buildfile install/dbdeploy/build.xml db:update $@
+sh install/ant/bin/ant $BUILD_OVERRIDES -buildfile install/build.xml db:update $@
 
