142f8c20f7ddb94e6525a5fb36cef377c6e9074b
Ugochi <uofoha@thoughtworks.com>
2011-04-07 10:29:47 +0530

    [UgochiO, PeterM] #259 Fix building error

--- src/main/java/org/sukrupa/student/SystemEventLogRepository.java
+++ src/main/java/org/sukrupa/student/SystemEventLogRepository.java
@@ -1,13 +1,12 @@
 package org.sukrupa.student;
 
 
-import org.mockito.stubbing.OngoingStubbing;
+//import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class SystemEventLogRepository {
-
 
 
     public SystemEventLog find(String lastRun) {
