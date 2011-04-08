f58fd66d46caabde6c2d4fe1ef118e9d54e5e587
Jim Barritt <jim@planet-ix.com>
2011-04-08 12:34:30 +0530

    [JimB, TC, SorayaH] Trying to fix stop server for staging

--- ops/deploy/stop-server.sh
+++ ops/deploy/stop-server.sh
@@ -11,6 +11,29 @@ findSukrupaServerProcess() {
     esac
 }
 
+findSukrupaServerProcessStaging() {
+    PID=""
+    case $(uname) in
+      Darwin | Linux)
+        PID=$(ps aux | grep java | grep org.sukrupa.app.SchoolAdminApp | grep -v ant-launcher.jar | awk '{ print $2 }')
+        ;;
+      *)
+       echo "Unrecognised OS, supported OS's are MacOSX, Linux & SunOS (Solaris)"
+       exit 1
+    esac
+}
+
+stopSukrupaServerStaging() {
+    findSukrupaServerProcess
+    if [ -z $PID ]; then
+        echo "No running server found, nothing to stop."
+    else
+        echo "Killing process with PID [${PID}]"
+        kill -9 $PID
+        echo
+        echo "Sukrupa Server stopped"
+    fi
+}
 
 stopSukrupaServer() {
     findSukrupaServerProcess
@@ -25,3 +48,4 @@ stopSukrupaServer() {
 }
 
 stopSukrupaServer
+stopSukrupaServerStaging
