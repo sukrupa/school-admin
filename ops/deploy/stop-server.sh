facc9b793e16fd7884f2bcd437d3c6fbc507729d
Jim Barritt <jim@planet-ix.com>
2011-04-09 11:56:20 +0530

    [JimB] Using PID to kill server instead of ps aux

--- ops/deploy/stop-server.sh
+++ ops/deploy/stop-server.sh
@@ -1,52 +1,24 @@
 #!/bin/bash
 findSukrupaServerProcess() {
-    PID=""
-    case $(uname) in
-      Darwin | Linux)
-        PID=$(ps aux | grep java | grep school-admin | grep -v org.hsqldb.server.Server | grep -v ant-launcher.jar | awk '{ print $2 }')
-        ;;
-      *)
-       echo "Unrecognised OS, supported OS's are MacOSX, Linux & SunOS (Solaris)"
-       exit 1
-    esac
+    PID=$(cat ~/.webserver.pid)
 }
 
-findSukrupaServerProcessByClassname() {
-    PID=""
-    case $(uname) in
-      Darwin | Linux)
-        PID=$(ps aux | grep java | grep org.sukrupa.app.SchoolAdminApp | grep -v ant-launcher.jar | awk '{ print $2 }')
-        ;;
-      *)
-       echo "Unrecognised OS, supported OS's are MacOSX, Linux & SunOS (Solaris)"
-       exit 1
-    esac
-}
 
-stopSukrupaServerClassname() {
-    findSukrupaServerProcessByClassname
-    if [ -z $PID ]; then
-        echo "No running server found with name 'school-admin', nothing to stop."
-    else
-        echo "Killing process with PID [${PID}]"
-        kill -9 $PID
-        echo
-        echo "Sukrupa Server stopped"
-    fi
-}
 
 stopSukrupaServer() {
     findSukrupaServerProcess
     if [ -z $PID ]; then
-        echo "No running server by classname found, nothing to stop."
+        echo "No running server by PID file ~/.webserver.pid found, nothing to stop."
     else
         echo "Killing process with PID [${PID}]"
         kill -9 $PID
+        rm ~/.webserver.pid
+
         echo
         echo "Sukrupa Server stopped"
     fi
 }
-JAVA_PROCESSES=$(ps aux | grep java | awk '{ print $1 "\t" $2 "\t" $9 "\t" $10 "\t" $11 "\t" $13 }')
-echo -e "Running processes:\n$JAVA_PROCESSES"
 stopSukrupaServer
-stopSukrupaServerClassname
+
+JAVA_PROCESSES=$(ps aux | grep java | grep -v grep |awk '{ print $1 "\t" $2 "\t" $9 "\t" $10 "\t" $11 }')
+echo -e "Running processes:\n$JAVA_PROCESSES"
--- src/main/java/org/sukrupa/platform/server/WebServer.java
+++ src/main/java/org/sukrupa/platform/server/WebServer.java
@@ -1,37 +1,21 @@
 package org.sukrupa.platform.server;
 
-import org.apache.log4j.Logger;
-import org.eclipse.jetty.http.security.Constraint;
-import org.eclipse.jetty.security.ConstraintMapping;
-import org.eclipse.jetty.security.ConstraintSecurityHandler;
-import org.eclipse.jetty.security.HashLoginService;
-import org.eclipse.jetty.security.authentication.FormAuthenticator;
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ErrorHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
-import org.eclipse.jetty.server.ssl.SslSocketConnector;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.resource.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jmx.support.ConnectorServerFactoryBean;
-import org.springframework.stereotype.Component;
-import org.sukrupa.platform.web.FrontController;
-import org.sukrupa.platform.web.ImageController;
+import org.apache.log4j.*;
+import org.eclipse.jetty.http.security.*;
+import org.eclipse.jetty.security.*;
+import org.eclipse.jetty.security.authentication.*;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.handler.*;
+import org.eclipse.jetty.servlet.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.sukrupa.platform.web.*;
 
+import java.io.*;
+import java.lang.management.*;
+import java.util.*;
 
-import javax.servlet.Servlet;
-import java.io.IOException;
-import java.net.Socket;
-import java.util.Arrays;
-
-
-import static java.lang.String.format;
+import static java.lang.String.*;
 
 @Component
 public class WebServer {
@@ -82,10 +66,58 @@ public class WebServer {
         try {
             LOG.info(format("Starting Web Server (web root:%s)...", webRoot));
             server.start();
+            writeProcessIdToFile();
             System.out.println("WebServer Started.");
             server.join();
         } catch (Exception e) {
             throw new WebServerStartupException(e);
+        }
+    }
+
+    private static void writeProcessIdToFile() {
+        FileOutputStream out = null;
+        try {
+            File processIdFile = new File(System.getProperty("user.home"), ".webserver.pid");
+            if (processIdFile.exists()) {
+                processIdFile.delete();
+            }
+            processIdFile.deleteOnExit();
+            processIdFile.createNewFile();
+
+            out = new FileOutputStream(processIdFile);
+
+            String processName = ManagementFactory.getRuntimeMXBean().getName();
+            if (!processName.contains("@")) {
+                throw new RuntimeException(format("Process name [%s] is not parsable for the processId!", processName));
+            }
+
+            String[] parts = processName.split("@");
+            String processId = parts[0];
+            String hostName = parts[1];
+
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(out));
+            writer.println(processId);
+            writer.flush();
+
+            System.out.println(format("Starting server with PID [%s] on [%s]", processId, hostName));
+            System.out.println(format("Process Id file is at [%s]", processIdFile.getAbsolutePath()));
+
+        } catch (IOException e) {
+            throw new RuntimeException("Could not write process Id file ~/.webserver.pid]", e);
+        } finally {
+            closeQuietly(out);
+        }
+    }
+
+    private static void closeQuietly(OutputStream out) {
+        if (out == null) {
+            return;
+        }
+
+        try {
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not close stream (See Cause)", e);
         }
     }
 
@@ -125,7 +157,7 @@ public class WebServer {
         ConstraintSecurityHandler securityHandler = new ConstraintSecurityHandler();
         securityHandler.setLoginService(server.getBean(HashLoginService.class));
 
-        FormAuthenticator authenticator = new FormAuthenticator("/authentication/login?success=true","/authentication/login?success=false",true);
+        FormAuthenticator authenticator = new FormAuthenticator("/authentication/login?success=true", "/authentication/login?success=false", true);
 
         securityHandler.setAuthenticator(authenticator);
 
