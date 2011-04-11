7c24042ff2377cfcb1e3702f60f1b6b640c3b0ee
Joel Tellez <Jtellez@thoughtworks.com>
2011-04-11 07:15:22 -0500

    [JoelT, MarkN] Fixed unit test in studenttest

--- src/test/unit/java/org/sukrupa/student/StudentTest.java
+++ src/test/unit/java/org/sukrupa/student/StudentTest.java
@@ -219,7 +219,7 @@ public class StudentTest {
         setOfEvents.add(backstreetBoys);
 
         Student student = new StudentBuilder().events(setOfEvents).build();
-        assertThat(student.getEventsForDisplay(), is("Backstreet Boys, Spice Girls"));
+        assertThat(student.getEventsForDisplay(), is("Spice Girls, Backstreet Boys"));
     }
 
     private Student promoteStudent(String studentClass) {
