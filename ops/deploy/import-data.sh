1901218c87d4dc7e3cf9ea80c39d74168db7ce01
Francisco Trindade <frank.trindade@gmail.com>
2011-04-12 09:48:49 +0530

    [UgochiO, FrankT] #354 Removing sql from codebase, updating import data scripts in deployment

---
+++ ops/deploy/import-data.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar install/importer/sukrupa_import.jar ../../sukrupa_import_data/import_data.properties
--- tools/sukrupa-import/import-data.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -jar sukrupa_import.jar /home/ubuntu/sukrupa_import_data/import_data.properties
