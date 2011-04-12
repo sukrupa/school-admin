62962becf63fe667b4724a8619d829d6450d0ca9
Francisco Trindade <frank.trindade@gmail.com>
2011-04-12 10:29:28 +0530

    [UgochiO, FrankT] #354 Fixing path for import data script

--- ops/deploy/import-data.sh
+++ ops/deploy/import-data.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar install/importer/sukrupa_import.jar ../../sukrupa_import_data/import_data.properties
+java -jar install/importer/sukrupa_import.jar ../sukrupa_import_data/import_data.properties
