2332099e856d6f70bf8ba9e71aeb2e366fae82bb
Peter Murray <pj.murray.nz@gmail.com>
2011-04-20 14:01:37 +0530

    [PeterM] Fixed destination of real data

--- tools/import-student-records/import_students.rb
+++ tools/import-student-records/import_students.rb
@@ -5,7 +5,7 @@ require 'student'
 require 'caregiver'
 require 'sql_generator'
   
-TARGET_SQL_FILE = '../../ops/deploy/sql/insert-fake-data.sql'
+TARGET_SQL_FILE = '../../ops/deploy/sql/data_import.sql'
 
 workbook = Excelx.new(ARGV[0])
 students_and_talents_array = WorkbookParser.new.parse(workbook)
