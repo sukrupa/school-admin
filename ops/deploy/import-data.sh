ef341fab791cb080e44fb9a9c702af0abce615de
Peter Murray <pj.murray.nz@gmail.com>
2011-04-20 14:52:55 +0530

    [BalajiN] #BUILD fixing the build (trying to)

--- ops/deploy/import-data.sh
+++ ops/deploy/import-data.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd ../; java -jar install/importer/sukrupa_import.jar ../sukrupa_import_data/import_data.properties
+java -jar install/importer/sukrupa_import.jar ../sukrupa_import_data/import_data.properties
