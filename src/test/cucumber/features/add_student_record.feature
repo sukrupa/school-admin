1edec26a2f03a4f5a4f6fcb9412ffd52f1b01b16
Nishitha Ningegowda <nningego@thoughtworks.com>
2011-06-29 12:53:50 +0530

    Nishi and Aravind : Refactored feature statements for more readability

--- src/test/cucumber/features/add_student_record.feature
+++ src/test/cucumber/features/add_student_record.feature
@@ -2,15 +2,21 @@
 Feature: Add student record
 
 @OnListOfStudentsPage
-      Scenario: Go to register a student page
-      When I click on "Add New Student"
-      Then I should see the "Register a student" Page
+     Scenario: Go to Register a Student page
+     When I "Add New Student" in the sidebar
+     Then the "Register a student" page is displayed
 
 @OnRegisterAStudentPage
-      Scenario: Add new student details
-      When I fill in the studentId with "SK2010082011"
-      And I fill in the name as "Yael"
-      And I fill in the dateOfBirth as "06-03-1982"
-      And I select the gender as "Female"
-      And I click "Register" submit button
-      Then I should see the "Edit Student Record: Yael" page
+     Scenario: Add new student details
+     When I enter the "studentId" as "SK2010082011"
+     And I enter the "name" as "Yael"
+     And I enter the "dateOfBirth" as "06-03-1982"
+     And I enter the "gender" as "Female"
+     And I click the "Register" button
+     Then the "Edit Student Record: Yael" page is displayed
+
+@OnRegisterAStudentPage
+     Scenario: Return from register a student page without adding a new student
+     When I click the "Cancel" button
+     Then the "List of Students" page is displayed
+
--- src/test/cucumber/java/org/sukrupa/cucumber/steps/AddStudentRecordSteps.java
+++ src/test/cucumber/java/org/sukrupa/cucumber/steps/AddStudentRecordSteps.java
@@ -8,52 +8,57 @@ import static org.sukrupa.cucumber.SahiFacade.browser;
 
 public class AddStudentRecordSteps extends BasicWebSteps {
 
-     private static final String TOP_LEVEL_DIV = "page";
+    // private static final String TOP_LEVEL_DIV = "page";
 
-    @When("^I click on \"([^\"]*)\"$")
-    public void iClickOnAddNewStudent_(String text) {
-        super.clickLink(text);
-    }
+//    @When("^I click on \"([^\"]*)\"$")
+//    public void iClickOnAddNewStudent_(String text) {
+//        super.clickLink(text);
+//    }
 
-    @Then("^I should see the \"([^\"]*)\" Page$")
-    public void iShouldSeeTheRegisterAStudentPage(String text) {
-        super.shouldSee(text);
-    }
-
-
-    @When("^I fill in the studentId with \"([^\"]*)\"$")
-    public void iFillInTheStudentIdWithSK2010082011_(String text) {
-        browser().textbox("studentId").setValue(text);
-    }
+//    @Then("^I should see the \"([^\"]*)\" Page$")
+//    public void iShouldSeeTheRegisterAStudentPage(String text) {
+//        super.shouldSee(text);
+//    }
 
 
-    @When("^I fill in the name as \"([^\"]*)\"$")
-    public void iFillInTheNameAsYael_(String text) {
-        browser().textbox("name").setValue(text);
+//    @When("^I fill in the studentId with \"([^\"]*)\"$")
+//    public void iFillInTheStudentIdWithSK2010082011_(String text) {
+//        browser().textbox("studentId").setValue(text);
+//    }
+//
+//
+//    @When("^I fill in the name as \"([^\"]*)\"$")
+//    public void iFillInTheNameAsYael_(String text) {
+//        browser().textbox("name").setValue(text);
+//
+//    }
+//
+//    @When("^I fill in the dateOfBirth as \"([^\"]*)\"$")
+//    public void iFillInTheDateOfBirthAs06_03_1982_(String text) {
+//        browser().textbox("dateOfBirth").setValue(text);
+//
+//    }
 
-    }
+//    @When("^I select the gender as \"([^\"]*)\"$")
+//    public void iSelectTheGenderAsFemale_(String text) {
+//
+//        browser().select("gender").choose(text);
+//    }
 
-    @When("^I fill in the dateOfBirth as \"([^\"]*)\"$")
-    public void iFillInTheDateOfBirthAs06_03_1982_(String text) {
-        browser().textbox("dateOfBirth").setValue(text);
-
-    }
-
-    @When("^I select the gender as \"([^\"]*)\"$")
-    public void iSelectTheGenderAsFemale_(String text) {
-
-        browser().select("gender").choose(text);
-    }
-
-    @When("^I click \"([^\"]*)\"$")
-    public void iClickRegister_(String text) {
-        super.clickSubmitButton(text);
-    }
-
-    @Then("^I should see the \"([^\"]*)\" page$")
-    public void iShouldSeeTheEditStudentRecordYaelPage(String text) {
-          super.shouldSee(text);
-    }
+//    @When("^I click \"([^\"]*)\"$")
+//    public void iClickRegister_(String text) {
+//        super.clickSubmitButton(text);
+//    }
+//
+//    @Then("^I should see the \"([^\"]*)\" page$")
+//    public void iShouldSeeTheEditStudentRecordYaelPage(String text) {
+//          super.shouldSee(text);
+//    }
+//
+//    @When("^I click on \"([^\"]*)\" button$")
+//    public void iClickOnCancelButton(String text) {
+//        super.clickButton(text);
+//    }
 
 
 }
