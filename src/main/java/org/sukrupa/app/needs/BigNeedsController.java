a69a936b83c56492f6e22d17615d2e8905f31d45
Pooja Akshantal <pooja.akshantal@yahoo.com>
2011-06-29 15:35:08 +0530

    Vini/Kishore/Anita: Story 543 removed unused classes

--- src/main/java/org/sukrupa/app/needs/BigNeedsController.java
+++ src/main/java/org/sukrupa/app/needs/BigNeedsController.java
@@ -1,23 +1,13 @@
 package org.sukrupa.app.needs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.sukrupa.need.BigNeed;
-import org.sukrupa.need.BigNeedService;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/bigneeds")
 public class BigNeedsController {
-    private BigNeedService bigNeedService;
-
-    @Autowired
-    public BigNeedsController(BigNeedService bigNeedService) {
-        this.bigNeedService = bigNeedService;
-    }
 
     @RequestMapping
     public String list(Map <String, Object> model) {
--- src/test/unit/java/org/sukrupa/app/needs/BigNeedsControllerTest.java
+++ src/test/unit/java/org/sukrupa/app/needs/BigNeedsControllerTest.java
@@ -2,8 +2,6 @@ package org.sukrupa.app.needs;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.sukrupa.need.BigNeedService;
 
 import java.util.HashMap;
 
@@ -12,13 +10,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BigNeedsControllerTest {
 
-    @Mock
-    private BigNeedService service;
     private BigNeedsController controller;
       private HashMap<String, Object> model = new HashMap<String, Object>();
     @Before
     public void setUp(){
-        controller = new BigNeedsController(service);
+        controller = new BigNeedsController();
     }
     @Test
     public  void shouldDisplayBigNeedsPage(){
