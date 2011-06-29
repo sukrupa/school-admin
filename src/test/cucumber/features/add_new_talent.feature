67241e8e0878afa7687ab54ca2a26fb7016d3a9c
Aleksandra Bondzic <abondzic@thoughtworks.com>
2011-06-29 00:09:12 -0500

    Commit changes to Add new talent cucumber tests

--- src/test/cucumber/features/add_new_talent.feature
+++ src/test/cucumber/features/add_new_talent.feature
@@ -4,7 +4,9 @@ Feature: Add new talent
     @OnAddNewTalentPage
     Scenario: Add new talent
        When I fill in the "description" with "firebreathing"
-       And I click "addNewTalent" button
-       And I click "OK" button
+       And I click "Add Talent" submit button
        Then I should see "added successfully"
+
+    @OnStudentIDAbhishekProfilePage
+    Scenario: Check new talent exists on StudentID 16346 edit page
 
--- src/test/cucumber/java/org/sukrupa/cucumber/context/OnAddNewTalentPage.java
+++ src/test/cucumber/java/org/sukrupa/cucumber/context/OnAddNewTalentPage.java
@@ -5,13 +5,6 @@ import org.sukrupa.cucumber.steps.BasicWebSteps;
 
 import static org.sukrupa.cucumber.SahiFacade.browser;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Thoughtworks
- * Date: 6/28/11
- * Time: 4:33 AM
- * To change this template use File | Settings | File Templates.
- */
 public class OnAddNewTalentPage extends BasicWebSteps {
 
     private final String homePage = "http://localhost:8080/students/";
@@ -22,4 +15,6 @@ public class OnAddNewTalentPage extends BasicWebSteps {
         browser().link("Tools").click();
         browser().link("Add New Talent").click();
     }
+
+
 }
