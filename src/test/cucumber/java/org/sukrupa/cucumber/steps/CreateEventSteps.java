ed95b773effdf85356fce3e53b80f508ea415589
Roberto <rgarcia@thoughtworks.com>
2011-06-29 10:51:39 +0530

    Roberto: working on create event feature test

--- src/test/cucumber/java/org/sukrupa/cucumber/steps/CreateEventSteps.java
+++ src/test/cucumber/java/org/sukrupa/cucumber/steps/CreateEventSteps.java
@@ -1,5 +1,21 @@
 package org.sukrupa.cucumber.steps;
 
 
-public class CreateEventSteps {
+import cuke4duke.annotation.I18n;
+import org.sukrupa.cucumber.context.Login;
+import sun.tools.tree.SuperExpression;
+
+public class CreateEventSteps extends BasicWebSteps {
+
+
+
+
+
+    @I18n.EN_AU.When("I fill in the Title with \"([^\"]*)\"$")
+    public void fillInTitle(String title){
+
+
+
+    }
+
 }
