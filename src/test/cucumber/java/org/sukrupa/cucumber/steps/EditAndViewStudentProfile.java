137565eb95e772b1ce0129de93b9f203b4a2abb5
Nishitha Ningegowda <nningego@thoughtworks.com>
2011-07-01 02:34:42 +0530

    mishi and shikhar: finished view_student_profile.feature

---
+++ src/test/cucumber/java/org/sukrupa/cucumber/steps/EditAndViewStudentProfile.java
@@ -0,0 +1,11 @@
+package org.sukrupa.cucumber.steps;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thoughtworks
+ * Date: 7/1/11
+ * Time: 1:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EditAndViewStudentProfile {
+}
---
+++ src/test/cucumber/java/org/sukrupa/cucumber/steps/ViewStudentListSteps.java
@@ -0,0 +1,11 @@
+package org.sukrupa.cucumber.steps;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thoughtworks
+ * Date: 7/1/11
+ * Time: 2:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ViewStudentListSteps {
+}
