df5352b508ace7e8a19ae0e75b4ca833580c9913
Nishitha Ningegowda <nningego@thoughtworks.com>
2011-06-30 14:29:53 +0530

    nishi and shikhar: working on edit student profile steps.feature

---
+++ src/test/cucumber/java/org/sukrupa/cucumber/steps/EditStudentProfileSteps.java
@@ -0,0 +1,11 @@
+package org.sukrupa.cucumber.steps;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thoughtworks
+ * Date: 6/30/11
+ * Time: 12:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EditStudentProfileSteps {
+}
