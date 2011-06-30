39670b1c2137b556fe72e954ffabc48f43523067
Steph Hof <shof@thoughtworks.com>
2011-06-30 11:35:46 +0530

    Steph/Pooja: refactored OnListofStudentPage to use properties file

--- src/test/cucumber/java/org/sukrupa/cucumber/context/OnListOfStudentsPage.java
+++ src/test/cucumber/java/org/sukrupa/cucumber/context/OnListOfStudentsPage.java
@@ -3,13 +3,14 @@ package org.sukrupa.cucumber.context;
 import cuke4duke.annotation.Before;
 import org.sukrupa.cucumber.steps.BasicWebSteps;
 
+import static org.sukrupa.cucumber.CucumberFacade.getConfigProperty;
 import static org.sukrupa.cucumber.SahiFacade.browser;
 
 public class OnListOfStudentsPage extends BasicWebSteps {
 
     @Before("@OnListOfStudentsPage")
     public void listOfStudentsPage(){
-        browser().navigateTo("http://localhost:8080/students");
+        browser().navigateTo(getConfigProperty("homepage"));
 
     }
 }
