4ca3ede69c5f9bc7125f1fbc298ae12ff4365c4e
Aleksandra Bondzic <abondzic@thoughtworks.com>
2011-06-29 23:41:17 -0500

    Added Student family status cucumber tests

---
+++ src/test/cucumber/java/org/sukrupa/cucumber/context/OnStudentBhavaniProfileEditPage.java
@@ -0,0 +1,11 @@
+package org.sukrupa.cucumber.context;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thoughtworks
+ * Date: 6/29/11
+ * Time: 5:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OnStudentBhavaniProfileEditPage {
+}
