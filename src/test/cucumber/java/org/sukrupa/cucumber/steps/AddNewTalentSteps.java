52106458d27a5f27ec7034bb44a5d0fc2e3fba65
Rachel Laycock <rachellaycock@gmail.com>
2011-07-04 12:29:56 +0530

    Coding dojo: Added step implementation for add_new_talent.feature

--- src/test/cucumber/java/org/sukrupa/cucumber/steps/AddNewTalentSteps.java
+++ src/test/cucumber/java/org/sukrupa/cucumber/steps/AddNewTalentSteps.java
@@ -3,11 +3,15 @@ import cuke4duke.annotation.I18n.EN.*;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.sukrupa.cucumber.SahiFacade.browser;
 
 
 public class AddNewTalentSteps {
 
-
+    @When("^I enter talent description as ([^\"]*)$")
+    public void enterTalent(String talentName) {
+        browser().byId("description").setValue(talentName);
+    }
 
 
 
