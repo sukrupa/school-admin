8ed401e53cea23c812e0d505bc0f2c27b35a800e
Nishitha Ningegowda <nningego@thoughtworks.com>
2011-07-04 12:08:02 +0530

    Nishi/Ben: further big needs feature cleanup; changed sahi jar used by intellij and added sahi source

--- src/test/cucumber/features/big_need.feature
+++ src/test/cucumber/features/big_need.feature
@@ -4,17 +4,14 @@ Feature: Add, Edit and Delete Big Needs
   Scenario: Big Needs List is Displayed
     Given I am on the Big Needs page
     Then "Big Needs" should be displayed
-    And "Item" should be displayed
-    And "Cost" should be displayed
-    And "Power Generator" should be displayed
-    And "50000" should be displayed
+    And a "Power Generator" costing "50000" should be displayed
+    And a "50000" costing "Power Generator" should not be displayed
 
   Scenario: Add item to Big Need list
     Given I am on the Big Needs page
-    When I enter "Refridgerator" as the "itemName"
+    When I enter "Refrigerator" as the "itemName"
     And I enter "20000" as the "cost"
     And I submit the "Add" form
-    Then "Refridgerator" should be displayed
-    And "20000" should be displayed
+    Then a "Refrigerator" costing "20000" should be displayed
 
 #Scenario: Edit item in the Big Need list
--- src/test/cucumber/java/org/sukrupa/cucumber/steps/BigNeedSteps.java
+++ src/test/cucumber/java/org/sukrupa/cucumber/steps/BigNeedSteps.java
@@ -4,8 +4,10 @@ import cuke4duke.annotation.Before;
 import cuke4duke.annotation.I18n.EN.*;
 import cuke4duke.annotation.Pending;
 import net.sf.sahi.client.Browser;
+import net.sf.sahi.client.ElementStub;
 import org.sukrupa.cucumber.context.Login;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.sukrupa.cucumber.CucumberFacade.getConfigProperty;
@@ -20,5 +22,20 @@ public class BigNeedSteps extends Login {
         browser().link("Big Needs").click();
     }
 
+    @Then("^a \"([^\"]+)\" costing \"([^\"]+)\" should be displayed")
+    public void itemWithCostShouldBeDisplayed(String itemName, String cost) {
+        assertTrue(itemWithCostExists(itemName, cost));
+    }
 
+    @Then("^a \"([^\"]+)\" costing \"([^\"]+)\" should not be displayed")
+    public void itemWithCostShouldNotBeDisplayed(String itemName, String cost) {
+        assertFalse(itemWithCostExists(itemName, cost));
+    }
+
+    private boolean itemWithCostExists(String itemName, String cost) {
+        ElementStub nameCellWithCorrectName = browser().cell(itemName).under(browser().cell("Item"));
+        ElementStub costCellWithCorrectCost = browser().cell(cost).under(browser().cell("Cost"));
+        ElementStub rowContainingBothNameAndCost = browser().cell(cost).in(browser().cell(itemName).parentNode());
+        return nameCellWithCorrectName.exists(true) && costCellWithCorrectCost.exists(true) && rowContainingBothNameAndCost.exists(true);
+    }
 }
