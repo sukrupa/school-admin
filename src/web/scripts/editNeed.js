6e08640f4e38726d822720dd9079693358fea6b5
Pooja Akshantal <pooja.akshantal@yahoo.com>
2011-07-07 16:25:41 +0530

    Pooja and Sree(#534):Prioritizing pipeline-display priority and default priority

--- src/web/scripts/editNeed.js
+++ src/web/scripts/editNeed.js
@@ -17,16 +17,18 @@ function saveRow(){
     var itemId = $(this).parents('tr').find('.item-id').val();
     var itemName = $(this).parents('tr').find('.item-name').val();
     var itemCost = $(this).parents('tr').find('.item-cost').val();
-    var params = {itemName: itemName, itemCost: itemCost, itemId:itemId};
+    var priority = $(this).parents('tr').find('.item-priority').val();
+    var params = {itemName: itemName, itemCost: itemCost, itemId:itemId, priority: priority};
     var url = "/bigneeds/saveeditedneed";
     jQuery.post(url, params);
     refreshPage();
 }
 
 function addBigNeed(){
+    var priority = $(this).parents('tr').find('.item-priority').val();
     var itemName = $(this).parents('tr').find('.item-name').val();
     var itemCost = $(this).parents('tr').find('.item-cost').val();
-    var params ={itemName: itemName, itemCost: itemCost};
+    var params ={itemName: itemName, itemCost: itemCost, priority: priority};
     var url = "/bigneeds/create";
     jQuery.post(url,params);
     refreshPage();
