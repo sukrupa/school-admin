660b3633e634c5811100fa7ae01fe586761a5d77
Abhinaya <abhijan90@gmail.com>
2011-07-14 12:31:03 +0530

    Abhi/Kishore: Fixing build,  added starttime in insert-fake-data

--- ops/deploy/sql/insert-fake-data.sql
+++ ops/deploy/sql/insert-fake-data.sql
@@ -107,24 +107,24 @@ VALUES (3,7);
 
 
 INSERT INTO "PUBLIC"."EVENT"
-("ID","TITLE", "VENUE","COORDINATOR","DESCRIPTION","NOTES","END_DATE")
+("ID","TITLE", "VENUE","COORDINATOR","DESCRIPTION","NOTES","START_DATE","END_DATE")
 VALUES
-(1,'Annual Day', 'Bangalore 2','Rebecca Sliter','This is a event description. It is a test.','This is a note','2011-02-20 00:00:00');
+(1,'Annual Day', 'Bangalore 2','Rebecca Sliter','This is a event description. It is a test.','This is a note','2011-01-20 00:23:00','2011-02-20 00:00:00');
 
 INSERT INTO "PUBLIC"."EVENT"
-("ID","TITLE", "VENUE","COORDINATOR","DESCRIPTION","NOTES","END_DATE")
+("ID","TITLE", "VENUE","COORDINATOR","DESCRIPTION","NOTES","START_DATE","END_DATE")
 VALUES
-(2,'Sports Day', 'Bangalore agra','Suhas Vishwanath','This is a event description. It is a test.','This is a note','2011-03-25 00:00:00');
+(2,'Sports Day', 'Bangalore agra','Suhas Vishwanath','This is a event description. It is a test.','This is a note','2011-01-20 00:00:23','2011-03-25 00:00:00');
 
 INSERT INTO "PUBLIC"."EVENT"
-("ID","TITLE", "VENUE","COORDINATOR","DESCRIPTION","NOTES","END_DATE")
+("ID","TITLE", "VENUE","COORDINATOR","DESCRIPTION","NOTES","START_DATE","END_DATE")
 VALUES
-(3,'Independence Day', 'Bangalore agra','Shefali','This is a event description. It is a test.','This is a note','2011-02-27 00:00:00');
+(3,'Independence Day', 'Bangalore agra','Shefali','This is a event description. It is a test.','This is a note','2011-02-20 00:00:00','2011-03-27 00:00:00');
 
 INSERT INTO "PUBLIC"."EVENT"
-("ID","TITLE", "VENUE","COORDINATOR","DESCRIPTION","NOTES","END_DATE")
+("ID","TITLE", "VENUE","COORDINATOR","DESCRIPTION","NOTES","START_DATE","END_DATE")
 VALUES
-(4,'Spice Girls', 'P-81 Diamond District','Joel Tellez','This is a event description. It is a test.','This is a note','2011-04-05 00:00:00');
+(4,'Spice Girls', 'P-81 Diamond District','Joel Tellez','This is a event description. It is a test.','This is a note','2011-02-20 00:00:00','2011-04-05 00:00:00');
 
 INSERT INTO EVENT_ATTENDEES ("EVENT_ID","STUDENT_ID") VALUES (1,1);
 INSERT INTO EVENT_ATTENDEES ("EVENT_ID","STUDENT_ID") VALUES (1,2);
